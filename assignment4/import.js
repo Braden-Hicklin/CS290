8e8903c9f1d7281af85139779eeee9c2615b6097
D4ddyL0ngL3gs <52402651+D4ddyL0ngL3gs@users.noreply.github.com>
2023-02-23 14:26:06 -0800

    Update import.js

--- assignment4/import.js
+++ assignment4/import.js
@@ -1,4 +1,8 @@
+// Used to flip the direction after each onclick event (highest-lowest to lowest-highest)
 let sortDirection = false;
+
+// Manually set dictionary that was originally planned to be automated using cheerio, axios, and express
+// More on the original design can be found in app.js and the readme file
 let tableData = [
     {gpu: 'RTX 4090', price: 1599, benchmark: 370, compareVal: 0},
     {gpu: 'RX 7900 XTX', price: 1070, benchmark: 256, compareVal: 0},
@@ -15,27 +19,32 @@ let tableData = [
     {gpu: 'Arc A380', price: 140, benchmark: 38, compareVal: 0}
 ];
 
+// Initializes the table with the data set on load event
 window.onload = () => {
     loadTableData(tableData);
 };
 
+// Loads the table using the tableData dictionary
 function loadTableData() {
     const tableBody = document.getElementById('tableData');
     let dataHtml = '';
-
+    
+    // Loops through the items on the data set and sets the compareVal variable using price/benchmark parsed to a number
     for(let data of tableData) {
         data["compareVal"] = ((data.price/data.benchmark)*100 | 10);
         dataHtml += `<tr><td>${data.gpu}</td><td>${data.price}</td><td>${data.benchmark}</td><td>${data.compareVal}</td></tr>`;
     }
-
+    
     tableBody.innerHTML = dataHtml;
 }
 
+// Sorting function 
 function sortColumn(columnName) {
     const dataType = typeof tableData[0][columnName];	
     sortDirection = !sortDirection;
     console.log(dataType);
-
+    
+    // Swaps the direction of the sort
     switch(dataType) {
         case 'number':
             sortNum(sortDirection, columnName);
@@ -45,6 +54,7 @@ function sortColumn(columnName) {
     loadTableData(tableData);
 }
 
+// Basic sorting function using in library array sorting command
 function sortNum(sort, columnName) {
     tableData = tableData.sort((p1, p2) => {
         return sort ? p1[columnName] - p2[columnName] : p2[columnName] -
